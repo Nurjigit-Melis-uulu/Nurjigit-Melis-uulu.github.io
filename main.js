dc69fb0ae21dc382ec3b12993b392f6773a2f358
Melis uulu Nurjigit <nurjigit.melis.uulu@gmail.com>
2018-12-24 13:36:23 +0600

    modified label in index.html

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let darkMod = document.querySelector('#Dark-page');
+let label = document.querySelector('label');
 
 
 document.documentElement.style.setProperty('--bgc-welcome-page', "to right, #23719e, #88dadd, #88dadd, #23719e"); 
@@ -6,8 +7,10 @@ document.documentElement.style.setProperty('--bgc-welcome-page', "to right, #237
 darkMod.addEventListener('change', function() {
     if(this.checked == true) {
         document.documentElement.style.setProperty('--bgc-welcome-page', "to right, rgb(62, 0, 104), rgb(99, 67, 206), rgb(99, 67, 206), rgb(62, 0, 104)");
+        label.style.color = "white";
     }
     else {
         document.documentElement.style.setProperty('--bgc-welcome-page', "to right, #23719e, #88dadd, #88dadd, #23719e");
+        label.style.color = "black";
     }
 });
