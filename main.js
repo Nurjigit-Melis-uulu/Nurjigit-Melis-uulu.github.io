d013a2070d076f0ca4682248427cab80fc2af5c8
Melis uulu Nurjigit <nurjigit.melis.uulu@gmail.com>
2018-12-29 15:19:37 +0600

    Added js code for menu. Added menu in index.html

--- main.js
+++ main.js
@@ -1,6 +1,30 @@
 let darkMod = document.querySelector('#Dark-page');
 let label = document.querySelector('label');
+const menuButton = document.querySelector('#menu-button');
+const arrayForButton = [1];
+let line1 = document.querySelector('.line-1'); 
+let line2 = document.querySelector('.line-2'); 
+let line3 = document.querySelector('.line-3'); 
+let menuUl = document.querySelector('#show-menu');
 
+menuButton.addEventListener('click', function() {
+    arrayForButton.push(1);
+    if (arrayForButton.length % 2 == 0) {
+        line1.style.transform = "rotate(45deg) translate(16px, 1px)";
+        line2.style.transform = "translateX(64px)";
+        line2.style.opacity = "0";
+        line3.style.transform = "rotate(-45deg) translate(16px, -1px)";
+        menuUl.style.transform = "translateY(0)";
+    }
+    else {
+        line1.style.transform = "rotate(0deg) translate(0)";
+        line2.style.transform = "translateX(0)";
+        line2.style.opacity = "1";
+        line3.style.transform = "rotate(0deg) translate(0)";
+        menuUl.style.transform = "translateY(-300px)";
+        arrayForButton.length = 1;
+    }
+});
 
 document.documentElement.style.setProperty('--bgc-welcome-page', "to right, #23719e, #88dadd, #88dadd, #23719e"); 
 
