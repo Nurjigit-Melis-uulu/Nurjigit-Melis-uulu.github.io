658faf31dea11c96e947ffb6d9c82bedba0ed9ab
Melis uulu Nurjigit <nurjigit.melis.uulu@gmail.com>
2019-01-05 23:27:06 +0600

    New site!

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ let line3 = document.querySelector('.line-3');
 let menuUl = document.querySelector('#show-menu');
 let menuBox = document.querySelector('.menu-box');
 let optionsPageBox = document.querySelector('.options'); 
+let nameBox = document.querySelector('.name-box');
 
 menuButton.addEventListener('click', function() {
     arrayForButton.push(1);
@@ -16,6 +17,7 @@ menuButton.addEventListener('click', function() {
         line3.style.transform = "rotate(-45deg) translate(16px, -1px)";
         menuUl.style.transform = "translateX(0)";
         menuUl.style.height = "auto";
+        nameBox.style.transform = "translate(-100%)";
     }
     else {
         line1.style.transform = "rotate(0deg) translate(0)";
@@ -25,11 +27,46 @@ menuButton.addEventListener('click', function() {
         menuUl.style.transform = "translateX(100%)";
         arrayForButton.length = 1;
         menuUl.style.height = "50px";
+        nameBox.style.transform = "translate(0)";
     }
 });
 
 document.documentElement.style.setProperty('--bgc-welcome-page', "to right, #23719e, #88dadd, #88dadd, #23719e"); 
 
+let inputFocus = document.querySelectorAll('input');
+let label = document.querySelectorAll('label');
+let email = document.querySelector('.email');
+
+email.addEventListener('change', function() {
+	email.className = "mail";
+});
+
+for (var i = 0; i <= inputFocus.length; i++) {
+	let parentInput = inputFocus[i].parentElement.classList;
+
+	inputFocus[i].addEventListener('input', function() {
+	if (this.value.length > 0) {
+		parentInput.add.classList = "hidden";
+		parentInput.remove.className = "visible";
+	}
+	else {
+		parentInput.add.className = "visible";
+		parentInput.remove.className = "hidden";
+	};
+
+	inputFocus[i].addEventListener('change', function() {
+
+		if (this.value.length > 0) {
+			inputFocus.className = "mistake";
+			label.className = "hidden";
+		}
+		else {
+			inputFocus.className = "good";
+			label.className = "visible";
+		};
+	});
+});
+}
 /*
 darkMod.addEventListener('change', function() {
     if(this.checked == true) {
