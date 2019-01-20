622a43cb66434e87f9ed87d68925cb2094360744
Melis uulu Nurjigit <nurjigit.melis.uulu@gmail.com>
2019-01-20 21:56:37 +0600

    Deleted one bag

--- main.js
+++ main.js
@@ -138,17 +138,17 @@ const content = document.querySelectorAll('.content');
 let activeLink = navLinks[0]; // Home
 
 navLinks.className = "";
-for (let i = 0; i < navLinks.length; i++) {
-    navLinks[i].className = "hover";
-}
-
-//active home page in begin
-activeLink.className = "hover-active";
-document
-  .getElementById(activeLink.href.split('#').pop())
-  .className = "active content";
 
 navLinks.forEach(function(navLink) {
+    for (let i = 0; i < navLinks.length; i++) {
+        navLinks[i].className = "hover";
+    }
+
+    //active home page in begin
+    activeLink.className = "hover-active";
+    document
+    .getElementById(activeLink.href.split('#').pop())
+    .className = "active content";
   navLink.addEventListener('click', function(event) {
     // Deactivate current page
     activeLink.className = "hover";
