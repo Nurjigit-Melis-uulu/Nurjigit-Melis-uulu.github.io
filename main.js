d0ee54c5be610bd8c52955e558ba5f41b23c307b
Melis uulu Nurjigit <nurjigit.melis.uulu@gmail.com>
2019-03-12 21:04:07 +0600

    Add js for Nav

--- main.js
+++ main.js
@@ -1,71 +1,70 @@
-const menuButton = document.querySelector('#menu-button');
+const menuButton = document.querySelector("#menu-button");
 const arrayForButtonNav = [1];
 const arrayForButtonAside = [1];
-let menuUl = document.querySelector('#show-menu');
-let menuBox = document.querySelector('.menu-box');
-let optionsPageBox = document.querySelector('.options');
-let inputFocus = document.querySelectorAll('input');
-let label = document.querySelectorAll('label');
-let email = document.querySelector('.email');
-let containerAllPages = document.querySelector('.container');
-let submitButton = document.querySelector('#submit');
-let ulInService = document.querySelector('.box-services ul');
-let corouselInService = document.querySelector('.corousel');
-let downButton = document.querySelector('#down');
-let upButton = document.querySelector('#up');
+let menuUl = document.querySelector("#show-menu");
+let menuBox = document.querySelector(".menu-box");
+let optionsPageBox = document.querySelector(".options");
+let inputFocus = document.querySelectorAll("input");
+let label = document.querySelectorAll("label");
+let email = document.querySelector(".email");
+let containerAllPages = document.querySelector(".container");
+let submitButton = document.querySelector("#submit");
+let ulInService = document.querySelector(".box-services ul");
+let corouselInService = document.querySelector(".carousel");
+let downButton = document.querySelector("#down");
+let upButton = document.querySelector("#up");
 let translateYForcorouselInService = 0;
 let translateY = 0;
 let valueArray = 0;
-let drawer = document.querySelector('nav .drawer');
+let drawer = document.querySelector("nav .drawer");
 let boll = false;
-let backDrop = document.querySelector('.back-drop');
+let backDrop = document.querySelector(".back-drop");
+let body = document.querySelector('body');
 
+backDrop.addEventListener("click", function() {
+  drawer.style.transform = "translate(-100%)";
+  backDrop.style.display = "none";
+});
 
-backDrop.addEventListener('click', function() {
+menuButton.addEventListener("click", function() {
+  boll = !boll;
+
+  if (boll) {
+    drawer.style.transform = "translateX(0)";
+    backDrop.style.display = "block";
+  } else {
     drawer.style.transform = "translate(-100%)";
-    backDrop.style.display = "none";
+  }
 });
 
-menuButton.addEventListener('click', function() {
-    boll = !boll;
-
-    if (boll) {
-        drawer.style.transform = "translateX(0)";
-        backDrop.style.display = "block";
-    } else {
-        drawer.style.transform = "translate(-100%)";
-    }
-});
-
-email.addEventListener('change', function () {
-    email.className = "mail";
+email.addEventListener("change", function() {
+  email.className = "mail";
 });
 
 for (var i = 0; i < inputFocus.length; i++) {
-    let parentInput = inputFocus[i].parentElement;
+  let parentInput = inputFocus[i].parentElement;
 
-    inputFocus[i].addEventListener('input', function () {
-        if (this.value.length > 0) {
-            parentInput.classList.add("visible");
-            parentInput.classList.remove("hidden");
-        } else {
-            parentInput.classList.add("hidden");
-            parentInput.classList.remove("visible");
-        }
-    });
-    inputFocus[i].addEventListener('change', function () {
-
-        if (this.value.length > 0) {
-            parentInput.classList.add("good");
-            parentInput.classList.remove("hidden");
-            parentInput.classList.add("visible");
-        } else {
-            parentInput.classList.add("mistake");
-            parentInput.classList.remove("good");
-            parentInput.classList.add("visible");
-        }
-    });
-};
+  inputFocus[i].addEventListener("input", function() {
+    if (this.value.length > 0) {
+      parentInput.classList.add("visible");
+      parentInput.classList.remove("hidden");
+    } else {
+      parentInput.classList.add("hidden");
+      parentInput.classList.remove("visible");
+    }
+  });
+  inputFocus[i].addEventListener("change", function() {
+    if (this.value.length > 0) {
+      parentInput.classList.add("good");
+      parentInput.classList.remove("hidden");
+      parentInput.classList.add("visible");
+    } else {
+      parentInput.classList.add("mistake");
+      parentInput.classList.remove("good");
+      parentInput.classList.add("visible");
+    }
+  });
+}
 /*
 for (let i = 0; i < links.length; i++) {
     links[i].addEventListener('click', function() {
@@ -94,81 +93,92 @@ for (let i = 0; i < links.length; i++) {
 */
 const lis = ulInService.children;
 
-upButton.addEventListener('click', function () {
-    translateY += 60;
-    valueArray -= 1;
+upButton.addEventListener("click", function() {
+  translateY += 60;
+  valueArray -= 1;
 
-    ulInService.style.transform = "translateY(" + translateY + "px)";
+  ulInService.style.transform = "translateY(" + translateY + "px)";
 
-    checkButtonInService()
+  checkButtonInService();
 });
-downButton.addEventListener('click', function () {
-    translateY -= 60;
-    valueArray += 1;
+downButton.addEventListener("click", function() {
+  translateY -= 60;
+  valueArray += 1;
 
-    ulInService.style.transform = "translateY(" + translateY + "px)";
+  ulInService.style.transform = "translateY(" + translateY + "px)";
 
-    checkButtonInService()
+  checkButtonInService();
 });
 
 if (valueArray == 0) {
-    upButton.disabled = true;
-    downButton.disabled = false;
+  upButton.disabled = true;
+  downButton.disabled = false;
 }
 if (valueArray == 4) {
-    downButton.disabled = true;
-    upButton.disabled = false;
+  downButton.disabled = true;
+  upButton.disabled = false;
 }
 
-const navLinks = document.querySelectorAll('nav a');
-const content = document.querySelectorAll('.content');
+const navLinks = document.querySelectorAll("nav a");
+const content = document.querySelectorAll(".content");
 
 let activeLink = navLinks[0]; // Home
 
 navLinks.className = "";
 
-navLinks.forEach(function (navLink) {
+navLinks.forEach(function(navLink) {
+  //active home page in begin
+  document.getElementById(activeLink.href.split("#").pop()).className =
+    "active content";
+  navLink.addEventListener("click", function(event) {
+    // Deactivate current page
+    activeLink.className = "";
+    document.getElementById(activeLink.href.split("#").pop()).className =
+      "content";
 
-    //active home page in begin
-    document
-        .getElementById(activeLink.href.split('#').pop())
-        .className = "active content";
-    navLink.addEventListener('click', function (event) {
-        // Deactivate current page
-        activeLink.className = "";
-        document
-            .getElementById(activeLink.href.split('#').pop())
-            .className = "content";
+    // Set new page
+    activeLink = this;
 
-        // Set new page
-        activeLink = this;
+    // Activate new page
+    activeLink.className = "active-link";
+    document.getElementById(activeLink.href.split("#").pop()).className =
+      "active content";
 
-        // Activate new page
-        activeLink.className = "active-link";
-        document
-            .getElementById(activeLink.href.split('#').pop())
-            .className = "active content";
-
-        event.preventDefault();
-    });
+    event.preventDefault();
+  });
 });
 
 function checkButtonInService() {
-    if (valueArray == 0) {
-        upButton.disabled = true;
-        downButton.disabled = false;
-    } else {
-        upButton.disabled = false;
-    }
-    if (valueArray == 4) {
-        downButton.disabled = true;
-        upButton.disabled = false;
-    } else {
-        downButton.disabled = false;
-    }
+  if (valueArray == 0) {
+    upButton.disabled = true;
+    downButton.disabled = false;
+  } else {
+    upButton.disabled = false;
+  }
+  if (valueArray == 4) {
+    downButton.disabled = true;
+    upButton.disabled = false;
+  } else {
+    downButton.disabled = false;
+  }
 
-    for (let i = 0; i < lis.length; i++) {
-        lis[i].className = "";
-        lis[valueArray].className = "active-li";
-    }
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].className = "";
+    lis[valueArray].className = "active-li";
+  }
 }
+body.addEventListener("mousewheel", function(event) {
+  let nav = document.querySelector("nav");
+
+  let positionNav = nav.getBoundingClientRect();
+  let positionDoc = body.getBoundingClientRect();
+
+  if (positionNav.top == positionDoc.top) {
+    nav.style.cssText = "position: absolute; top: 0; left: 0; box-shadow: none; background: transparent; transition: all .4s linear;";
+    console.log(nav.getBoundingClientRect());
+  }
+  if (positionNav.bottom < 0) {
+    nav.style.cssText = "position: fixed; top: 0; left: 0; box-shadow: 0 3px 6px rgba(0, 0, 0, 0.2); background: #16263f; transition: all .1s linear;";
+    console.log(nav.getBoundingClientRect());
+  }
+});
