bcae9fafd66d21854816b2c27fb86c4eac75ca4b
Nurjigit <45354207+Nurjigit-Melis-uulu@users.noreply.github.com>
2019-11-06 22:11:16 +0600

    began stylized header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,18 @@ import React from "react";
 import classes from "./Header.module.css";
 
 function Header(props) {
-  return <div className={classes.Header}>Header</div>;
+  return (
+    <div className={classes.Header}>
+      <div>
+        <p>
+          <span>Welcome! My name is</span>
+          <span>Nurjigit Melis uulu</span>
+          <span>and i am</span>
+          <span>Front-End developer</span>
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
