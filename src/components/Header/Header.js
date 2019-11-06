6ac904c332ca6e1ddc6fd11b83bced9658e24b08
Nurjigit <45354207+Nurjigit-Melis-uulu@users.noreply.github.com>
2019-11-06 23:09:46 +0600

    added animation to header title

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,10 +5,10 @@ function Header(props) {
   return (
     <div className={classes.Header}>
       <div>
-        <p>
-          <span>Welcome! My name is</span>
+        <p className={classes.homePageTitle}>
+          <span>&mdash;&mdash; Welcome! My name is &mdash;&mdash;</span>
           <span>Nurjigit Melis uulu</span>
-          <span>and i am</span>
+          <span>&mdash;&mdash; and i am &mdash;&mdash;</span>
           <span>Front-End developer</span>
         </p>
       </div>
