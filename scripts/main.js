f7b93204d719dabca58ab338821b4431fbc5f807
Nurjigit <45354207+Nurjigit-Melis-uulu@users.noreply.github.com>
2020-01-16 23:14:19 +0600

    Modified link hover style with js

--- scripts/main.js
+++ scripts/main.js
@@ -3,5 +3,37 @@ let links = document.querySelectorAll("nav a");
 for (let i = 0; i < links.length; i++) {
   const element = links[i];
 
-  element.addEventListener("click", function() {});
+  element.addEventListener("mousemove", function(e) {
+    if (element.getAttribute("href") !== "#") {
+      for (let a = 0; a < links.length; a++) {
+        links[a].style.opacity = 0.5;
+      }
+      document.querySelector('a[href="#"]').style.opacity = 1;
+      this.style.opacity = 1;
+    }
+  });
+
+  element.addEventListener("mouseleave", function() {
+    for (let a = 0; a < links.length; a++) {
+      links[a].style.opacity = 1;
+    }
+  });
+
+  element.addEventListener("click", function(e) {
+    e.preventDefault();
+
+    const blockID = element.getAttribute("href");
+
+    if (element.getAttribute("href") === "#") {
+      document.querySelector("body").scrollIntoView({
+        behavior: "smooth",
+        block: "start"
+      });
+    } else {
+      document.querySelector("" + blockID).scrollIntoView({
+        behavior: "smooth",
+        block: "start"
+      });
+    }
+  });
 }
